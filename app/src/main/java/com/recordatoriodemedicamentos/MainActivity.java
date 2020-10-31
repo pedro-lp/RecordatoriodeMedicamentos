b1d1e3fb3e06ccdc3103fda9a349aee9a07c55c0
pedro-lp <pedro-lp@users.noreply.github.com>
2020-10-31 10:57:22 -0600

    Inicio Sesion

--- app/src/main/java/com/recordatoriodemedicamentos/MainActivity.java
+++ app/src/main/java/com/recordatoriodemedicamentos/MainActivity.java
@@ -3,12 +3,16 @@ package com.recordatoriodemedicamentos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText correo, contra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        correo = findViewById(R.id.txtUsuario);
+        contra = findViewById(R.id.txtContra);
     }
 }
