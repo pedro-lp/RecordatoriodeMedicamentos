1cb2ad6ea36ba97d9bcf7153c0217a948e425f86
Alexis Catalan <L16520290@chilpancingo.tecnm.mx>
2020-11-06 14:50:06 -0600

    main

--- app/src/main/java/com/recordatoriodemedicamentos/MainActivity.java
+++ app/src/main/java/com/recordatoriodemedicamentos/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        correo = findViewById(R.id.txtUsuario);
-        contra = findViewById(R.id.txtContra);
+        //correo = findViewById(R.id.txtUsuario);
+        //contra = findViewById(R.id.txtContra);
     }
 }
