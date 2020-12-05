1d1c56b361c6c6417d681267497acf71601bb9a4
Alexis Catalan <L16520290@chilpancingo.tecnm.mx>
2020-12-05 04:43:15 -0600

    main

---
+++ app/src/main/java/com/recordatoriodemedicamentos/SelectOptionActivity.java
@@ -0,0 +1,45 @@
+package com.recordatoriodemedicamentos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SelectOptionActivity extends AppCompatActivity {
+    Button btnLogin;
+    Button btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_option);
+        MyToolbar.show(this, "Seleccionar opción", true);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Register();
+            }
+        });
+    }
+
+    public void Login() {
+        Intent intent = new Intent(SelectOptionActivity.this, PerfilUsuario.class);
+        startActivity(intent);
+    }
+
+    public void Register() {
+        Intent intent = new Intent(SelectOptionActivity.this, PerfilUsuario.class);
+        startActivity(intent);
+    }
+}
