39539db569d5117b82de7ae43f628c619c71672b
pedro-lp <pedro-lp@users.noreply.github.com>
2021-01-15 17:17:39 -0600

    notificacion 5:17

--- app/src/main/java/com/recordatoriodemedicamentos/Notificacion/Recordatorios.java
+++ app/src/main/java/com/recordatoriodemedicamentos/Notificacion/Recordatorios.java
@@ -6,33 +6,43 @@ import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.recordatoriodemedicamentos.Modelo.MedicamentoProvider;
 import com.recordatoriodemedicamentos.R;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 public class Recordatorios extends AppCompatActivity {
     private TextView notificationsTime;
     private int alarmID = 1;
     private SharedPreferences settings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recordatorios);
 
+        //se crean las configuraciones de la notificacion
         settings = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
         String hour, minute;
-        hour = settings.getString("hour","");
-        minute = settings.getString("minute","");
+        //se
+        hour = settings.getString("hour", "");
+        minute = settings.getString("minute", "");
         notificationsTime = (TextView) findViewById(R.id.notifications_time);
 
-        if(hour.length() > 0){
+        if (hour.length() > 0) {
             notificationsTime.setText(hour + ":" + minute);
         }
+        //accion que se realiza al hacer clic en el picker
         findViewById(R.id.change_notification).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,9 +54,12 @@ public class Recordatorios extends AppCompatActivity {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
                         String finalHour, finalMinute;
+                        selectedHour = 13;
+                        selectedMinute = 00;
 
                         finalHour = "" + selectedHour;
                         finalMinute = "" + selectedMinute;
+
                         if (selectedHour < 10) finalHour = "0" + selectedHour;
                         if (selectedMinute < 10) finalMinute = "0" + selectedMinute;
                         notificationsTime.setText(finalHour + ":" + finalMinute);
@@ -55,13 +68,32 @@ public class Recordatorios extends AppCompatActivity {
 
                         today.set(Calendar.HOUR_OF_DAY, selectedHour);
                         today.set(Calendar.MINUTE, selectedMinute);
+                        ArrayList<Long> fecha = new ArrayList<>();
+                        today.set(Calendar.HOUR_OF_DAY, 13);
+                        today.set(Calendar.MINUTE, 49);
+                        today.set(Calendar.SECOND, 0);
+                        fecha.add(today.getTimeInMillis());
+
+                        today.set(Calendar.HOUR_OF_DAY, 13);
+                        today.set(Calendar.MINUTE, 51);
+                        today.set(Calendar.SECOND, 0);
+                        fecha.add(today.getTimeInMillis());
+
+                        today.set(Calendar.HOUR_OF_DAY, 13);
+                        today.set(Calendar.MINUTE, 36);
                         today.set(Calendar.SECOND, 0);
 
+                        fecha.add(today.getTimeInMillis());
+                        //se asigna la hora
                         SharedPreferences.Editor edit = settings.edit();
                         edit.putString("hour", finalHour);
                         edit.putString("minute", finalMinute);
+                        //SharedPreferences.Editor edit = settings.edit();
+                        //edit.putString("hour", finalHour);
+                        //edit.putString("minute", finalMinute);
 
                         //SAVE ALARM TIME TO USE IT IN CASE OF REBOOT
+                        //guarda la notificacion en caso de reiniciarse
                         edit.putInt("alarmID", alarmID);
                         edit.putLong("alarmTime", today.getTimeInMillis());
 
@@ -70,6 +102,14 @@ public class Recordatorios extends AppCompatActivity {
                         Toast.makeText(Recordatorios.this, getString(R.string.changed_to, finalHour + ":" + finalMinute), Toast.LENGTH_LONG).show();
 
                         Utils.setAlarm(alarmID, today.getTimeInMillis(), Recordatorios.this);
+                        //crea la alarma
+                        //Utils.setAlarm(alarmID, today.getTimeInMillis(), Recordatorios.this);
+                        for (int i = 0; i < 2; i++) {
+                            Toast.makeText(Recordatorios.this, MedicamentoProvider.medicamentosList.get(i).getNombre(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(Recordatorios.this, MedicamentoProvider.medicamentosList.get(i).getPriToma(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(Recordatorios.this, MedicamentoProvider.medicamentosList.get(i).getDuracion(), Toast.LENGTH_SHORT).show();
+                            Utils.setAlarm(i, fecha.get(i), Recordatorios.this);
+                        }
                     }
                 }, hour, minute, true);//Yes 24 hour time
                 mTimePicker.setTitle(getString(R.string.select_time));
@@ -77,10 +117,33 @@ public class Recordatorios extends AppCompatActivity {
 
             }
         });
+        //accion que realizar el boton detener la notificacion
         findViewById(R.id.stop_notification).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NotificationService.mp.stop();
+                try {
+                    //AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+                    //long updateInterval = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
+                    CharSequence hoy1 = DateFormat.format("yyyy-MM-dd", new Date().getTime());
+                    Toast.makeText(Recordatorios.this, "Hoy es;" + hoy1, Toast.LENGTH_LONG).show();
+                    CharSequence ultimoDia1 = ("2021-01-15");
+
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+                    Date hoy = dateFormat.parse(hoy1.toString());
+                    Date ultimoDia = dateFormat.parse(ultimoDia1.toString());
+
+                    Toast.makeText(Recordatorios.this, "Se desactivo la alarma", Toast.LENGTH_LONG).show();
+
+                    if (ultimoDia.before(hoy)) {
+                        Toast.makeText(Recordatorios.this, "la fecha de este medicamento ya vencio;" + ultimoDia, Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(Recordatorios.this, "El ultimo dia de tomar el medicamento es;" + ultimoDia, Toast.LENGTH_LONG).show();
+                        Utils.setAlarm(alarmID, System.currentTimeMillis() + (5 * 60 * 1000), Recordatorios.this);
+                    }
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
